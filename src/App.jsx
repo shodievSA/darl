e7aa2a6ee816401c37a495c9c8bcd92e13f56ca8
Abbos Shodiev <shodiev.s.a@gmail.com>
2024-09-06 11:59:33 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./output.css"
+
+function App() {
+    return (
+        <>
+        <h1>Project Description Generator</h1>
+        </>
+    )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+  ],
+}
+
