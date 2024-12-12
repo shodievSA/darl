fd095200d1aeab0fba6ad7eaadf0550346d5703e
Abbos Shodiev <shodiev.s.a@gmail.com>
2024-12-12 22:30:05 +0500

    modified css styles

--- client/src/pages/Registration/Registration.jsx
+++ client/src/pages/Registration/Registration.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import styles from "./Registration.module.css";
 
 function Registration() {
+    const [isSigninButtonClicked, setIsSigninButtonClicked] = useState(false);
     function authorizeUser() {
+        setIsSigninButtonClicked(true);
+
         const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
         window.location.href = `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}`;
     }
@@ -14,21 +18,31 @@ function Registration() {
                 <h1>Generate professional descriptions of projects for your CV</h1>
             </div>
             <div className={styles['sign-in']}>
-                <button 
-                data-theme="halloween" 
-                className={styles['sign-in-button']}
-                onClick={authorizeUser}
-                >
-                    <div>
-                        <div>
-                            <FontAwesomeIcon 
-                            icon={faGithub} 
-                            className={styles['github-icon']}
-                            />
+                {
+                    isSigninButtonClicked ? (
+                        <div
+                        className={styles['loading-container']}
+                        >
+                            <span className="loading loading-spinner loading-lg"></span>
                         </div>
-                        <div>Sign in with GitHub</div>
-                    </div>
-                </button>
+                    ) : (
+                        <button 
+                        data-theme="halloween" 
+                        className={styles['sign-in-button']}
+                        onClick={authorizeUser}
+                        >
+                            <div>
+                                <div>
+                                    <FontAwesomeIcon 
+                                    icon={faGithub} 
+                                    className={styles['github-icon']}
+                                    />
+                                </div>
+                                <div>Sign in with GitHub</div>
+                            </div>
+                        </button>
+                    )
+                }
                 <p className={styles['important']}>
                     We will need to get access to your public repositories 
                     to generate descriptions. We will be permitted to only 
