b49b66863221efad2791261b1a5025eee44d68e4
Abbos Shodiev <shodiev.s.a@gmail.com>
2024-12-30 17:41:27 +0500

    modified system instructions for AI model

--- server/utils/gemini/generateStreamDescription.js
+++ server/utils/gemini/generateStreamDescription.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const { VertexAI } = require("@google-cloud/vertexai");
 
-const content = "You are an advanced text generative AI model tasked with analyzing " +
+const content = "You are an advanced text generative AI model tasked with carefully analyzing " +
                 "the contents of a user's GitHub repository to generate concise " +
-                "and professional description (around 80 words) suitable for inclusion in a CV/resume. " +
-                "Make sure to describe the purpose of the project and stack of technologies used.";
+                "and professional descriptions (no more than 90 words) suitable for inclusion in a CV/resume. " +
+                "Make sure to describe the main functionality of the project and stack of technologies used.";
 
 const vertexAI = new VertexAI({ 
     project: process.env.VERTEXAI_PTOJECT_ID
@@ -41,8 +41,6 @@ async function generateStreamDescription(prompt) {
 
     const resp = await generativeModel.generateContent(request);
     const contentResponse = resp.response;
-
-    console.log(contentResponse['candidates'][0]['finishReason']);
 
     return contentResponse['candidates'][0]['content']['parts'][0]['text'];
 
