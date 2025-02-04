1ea956bee88ad9463637c06f3f9b1774170a4604
Abbos Shodiev <shodiev.s.a@gmail.com>
2025-02-04 22:16:57 +0500

    added ability to generate multiple logos

--- server/database/addNewLogo.js
+++ server/database/addNewLogo.js
@@ -2,29 +2,39 @@ const { sequelize } = require("./models/user.js");
 const getUserFreeTrials = require("./getUserFreeTrials.js");
 const formatDate = require("../utils/formatDate.js");
 
-async function addNewLogo(userID, repoName) {
+async function addNewLogo(userID, repoName, logoQuantity) {
 
     const userFreeTrials = await getUserFreeTrials(userID);
 
-    const newLogo = {
-        value: `${userID}/${repoName}-${Date.now()}.png`,
-        repoName: repoName,
-        date: formatDate(),
-        type: 'logo',
-        price: userFreeTrials > 0 ? 'free trial' : 0.8
+    let newLogos = [];
+
+    for (let i = 0; i < logoQuantity; i++) {
+
+        const uniqueID = `${Date.now()}-${i}`;
+
+        const newLogo = {
+            value: `${userID}/${repoName}-${uniqueID}.png`,
+            repoName: repoName,
+            date: formatDate(),
+            type: 'logo',
+            price: userFreeTrials > 0 ? 'free trial' : 0.8
+        }
+
+        newLogos.push(newLogo);
+
     }
 
     await sequelize.query(
-        `UPDATE users SET user_history = :newLogo || user_history WHERE user_id = :userID`,
+        `UPDATE users SET user_history = :newLogos || user_history WHERE user_id = :userID`,
         {
             replacements: {
-                newLogo: JSON.stringify([newLogo]),
+                newLogos: JSON.stringify(newLogos),
                 userID: userID,
             },
         }
     );
 
-    return newLogo;
+    return newLogos;
 
 }
 
